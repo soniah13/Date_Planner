1d6ba647a55dc0da6f731773af4f5646525b5009
Imorgan Sonia <imorgansonia@gmail.com>
2025-09-04 08:34:20 +0300

    Add function to add a date plan and save to json

--- Planner/plans.py
+++ Planner/plans.py
@@ -14,10 +14,24 @@ def save_plans(plans):
     with open(DATA_FILE, 'w') as f:
         json.dump(plans, f, indent=2)
 
+def add_plans(title):
+    plans = load_plans()
+    new_plan = {"id":len(plans)+1, "title":title, "status":"Plan made"}
+    plans.append(new_plan)
+    save_plans(plans)
+    print(f"Added: {title} (Plan made)")
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python plans.py [add|list]")
         return
+    command = sys.argv[1].lower()
+    if command == "add":
+        if len(sys.argv) < 3:
+            print("Usage: python plans.py add \'Ice cream date\'")
+            return
+        title = " ".join(sys.argv[2:])
+        add_plans(title)
 
 if __name__ == "__main__":
     main()
